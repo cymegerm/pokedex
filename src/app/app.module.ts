46d2eeaf0b4b28db9a1de13d66e17f50a6843038
Simon St-Germain <cymegerm@gmail.com>
2020-09-08 04:02:09 -0400

    chore(lint): reformat files with linting issues using prettier

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,15 +6,9 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    instrumentStoreDevtools(),
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, instrumentStoreDevtools()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
